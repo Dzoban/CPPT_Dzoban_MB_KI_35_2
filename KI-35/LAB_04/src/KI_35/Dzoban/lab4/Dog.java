973a89875f691515767187e12f1cecadb073e670
Dzoban <dzeban03@gmail.com>
2022-10-09 22:43:52 +0300

    Lab_04

---
+++ KI-35/LAB_04/src/KI_35/Dzoban/lab4/Dog.java
@@ -0,0 +1,184 @@
+package KI_35.Dzoban.lab4;
+import java.io.*;
+import static java.lang.System.*;
+/** Class Dog implements your own pet
+ *
+ * @author Mykola Dzoban
+ * @version 1.0.3
+ * @since version 1.0.1
+ *
+ */
+abstract class Dog {
+    private Features fea;
+    private Opportunities opps;
+    private Walk walk;
+
+    private PrintWriter fout;
+    /**
+     * Constructor
+     * @throws FileNotFoundException check file existing
+     */
+    public Dog() throws FileNotFoundException
+    {
+        fea = new Features();
+        opps = new Opportunities();
+        walk = new Walk();
+        fout = new PrintWriter(new File("Dog.txt"));
+    }
+
+    /**
+     * Method makeDog implements making your own pet
+     * @param hp The health value
+     * @param en The energy value
+     * @param age The age value
+     */
+    public void makeDog(int hp, int en, int age){
+        fea.setHealth(hp);
+        fea.setEnergy(en);
+        fea.setAge(age);
+        fout.print("Dog health: "+fea.getHealth()+"\n");
+        fout.print("Dog energy: "+fea.getEnergy()+"\n");
+        fout.print("Dog age: "+fea.getAge()+"\n");
+        fout.flush();
+    }
+
+    /**
+     * Method aboutDog shows information about dog
+     */
+    public void aboutDog(){
+        out.print("Dog health: "+fea.getHealth()+"\n");
+        out.print("Dog energy: "+fea.getEnergy()+"\n");
+        out.print("Dog age: "+fea.getAge()+"\n");
+    }
+    /**
+     * Method dogHealth return information about dog's health
+     * @return The health value
+     */
+    public int dogHealth(){
+        return fea.getHealth();
+    }
+
+    /**
+     * Method makeAgeBigger implements rising age of dog
+     * @param age
+     */
+    public void makeAgeBigger(int age){
+        fea.changeAgeToGrow(age);
+        out.print("Dog grows: "+fea.getAge()+"\n");
+        fout.print("Dog grows: "+fea.getAge()+"\n");
+        fout.flush();
+    }
+
+    /**
+     * Method oppDog implements dog's opportunities
+     * @param hun The hungry value
+     * @param dream The dream value
+     * @param wash The purity value
+     */
+    public void oppDog(int hun, int dream, int wash){
+        opps.setHunger(hun);
+        opps.setDream(dream);
+        opps.setWash(wash);
+        fout.print("Hunger: "+opps.getHunger()+"\n");
+        fout.print("Dream: "+opps.getDream()+"\n");
+        fout.print("Purity: "+opps.getWash()+"\n");
+        fout.flush();
+    }
+
+    /**
+     * Method dogOpp shows information about dog's opportunities
+     */
+    public void dogOpp(){
+        out.print("Hunger: "+opps.getHunger()+"\n");
+        out.print("Dream: "+opps.getDream()+"\n");
+        out.print("Purity: "+opps.getWash()+"\n");
+    }
+
+    /**
+     * Method feedDog implements the possibility to feed dog
+     * @param food The food value
+     */
+    public void feedDog(int food){
+        opps.Feed(food);
+        fout.print("Dog's hunger: " + opps.getHunger() + "\n");
+        fout.flush();
+    }
+
+    /**
+     * Method sleepDog implements the possibility to dog to sleep
+     * @param hour The hour of dream value
+     */
+    public void sleepDog(int hour){
+        opps.Sleep(hour);
+        fout.print("Dog's dream: " + opps.getDream() + "\n");
+        fout.flush();
+    }
+
+    /**
+     * Method washDog implements the possibility to make dog clear
+     * @param wash The purity value
+     */
+    public void washDog(int wash){
+        opps.Wash(wash);
+        fout.print("Dog's purity: " + opps.getWash() + "\n");
+        fout.flush();
+    }
+
+    /**
+     * Method moveDog implements dog's moving
+     * @param xPos The X coordinate value
+     */
+    public void moveDog(int xPos){
+        walk.setXCoordinate(xPos);
+        fout.print("New dog position: " + walk.getXCoordinate() + "\n");
+        fout.flush();
+    }
+
+    /**
+     * Method getDogPosition returns dog's position
+     * @return X coordinate value
+     */
+    public int getDogPosition(){
+        return walk.getXCoordinate();
+    }
+
+    /**
+     * Method walkLeftWithDog implements the possibility to move dog left
+     * @param xPos The X coordinate value
+     */
+    public void walkLeftWithDog(int xPos){
+        walk.walkLeft(xPos);
+        out.print("Walk left position: " + walk.getXCoordinate() + "\n");
+        fout.print("Walk left position: " + walk.getXCoordinate() + "\n");
+        fout.flush();
+    }
+    /**
+     * Method walkRightWithDog implements the possibility to move dog right
+     * @param xPos The X coordinate value
+     */
+    public void walkRightWithDog(int xPos){
+        walk.walkRight(xPos);
+        out.print("Walk right position: " + walk.getXCoordinate() + "\n");
+        fout.print("Walk right position: " + walk.getXCoordinate() + "\n");
+        fout.flush();
+    }
+
+    /**
+     * Method dogJump implements the possibility for dog to jump
+     * @param xPos The X coordinate value
+     * @param yPos The Y coordinate value
+     */
+    public void dogJump(int xPos, int yPos){
+        walk.jump(xPos,yPos);
+        out.print("Dog jump on: " + walk.getXCoordinate() + ", " + walk.getYCoordinate()+"\n");
+        fout.print("Dog jump on: " + walk.getXCoordinate() + ", " + walk.getYCoordinate()+"\n");
+        fout.flush();
+    }
+    /**
+     * Method close file
+     */
+    public void dis()
+    {
+        fout.close();
+    }
+}
---
+++ KI-35/LAB_04/src/KI_35/Dzoban/lab4/Features.java
@@ -0,0 +1,73 @@
+package KI_35.Dzoban.lab4;
+/** Class Features implements the features of the pet
+ *
+ * @author Mykola Dzoban
+ * @version 1.0.3
+ * @since version 1.0.1
+ *
+ */
+public class Features {
+    //
+    private int health, energy, age;
+
+    /**
+     * Constructor
+     */
+    public Features(){
+        health = 0;
+        energy = 0;
+        age = 0;
+    }
+    /**
+     * Method returns the health value
+     * @return The health value
+     */
+    public int getHealth(){
+        return health;
+    }
+    /**
+     * Method returns the energy value
+     * @return The energy value
+     */
+    public int getEnergy(){
+        return energy;
+    }
+
+    /**
+     * Method returns the age value
+     * @return The age value
+     */
+    public int getAge() {
+        return age;
+    }
+    /**
+     * Method sets the health value
+     * @param hp The health value
+     */
+    public void setHealth(int hp){
+        this.health = hp;
+    }
+    /**
+     * Method sets the energy value
+     * @param energy The energy value
+     */
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    /**
+     * Method sets the age value
+     * @param age The age value
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    /**
+     * Method sets the age value after growing
+     * @param age The age value
+     */
+    public void changeAgeToGrow(int age){
+        this.age += age;
+    }
+
+}
